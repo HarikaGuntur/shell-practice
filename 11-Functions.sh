3979e7312ee701a18114ab8c6834f916d762b79a
Harika Guntur <hrknd4@gmail.com>
2026-03-09 12:25:56 +0530

    new file added

--- 11-Functions.sh
+++ 11-Functions.sh
@@ -7,7 +7,7 @@ if [ $USERID -ne 0 ]; then
     exit 1
 fi
 
-Validate(){
+validate(){
 
 if [ $1 -ne 0 ]; then
     echo "$2 ... FAILURE"
@@ -17,15 +17,12 @@ else
 fi
 }
 
-
 dnf install nginx -y
-validate $? "installing nginx"
-
-
+Validate $? "installing nginx"
 
 dnf install mysql -y
-validate $? "installing mysql"
+Validate $? "installing mysql"
 
 dnf install nodejs -y
-validate $? "installing nodejs"
+Validate $? "installing nodejs"
 
---
+++ 12-logs.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER=" /var/logs/shell-script"
+LOG_FILE=" /var/logs/shell-script/$0.log"
+
+if [ $USERID -ne 0 ]; then
+    echo "Please run this script with root user access"
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+Validate(){
+
+if [ $1 -ne 0 ]; then
+    echo "$2 ... FAILURE"
+    exit 1
+else
+    echo "$2 ... SUCCESS"
+fi
+}
+
+
+dnf install nginx -y $>> $LOG_FILE
+validate $? "installing nginx"
+
+dnf install mysql -y$ >> $LOG_FILE
+Validate $? "installing mysql"
+
+dnf install nodejs -y $>> $LOG_FILE
+Validate $? "installing nodejs"
