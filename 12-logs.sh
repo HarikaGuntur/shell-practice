7415c9e83a5b8e81e7085a55c878d29cbf0dd4e6
Harika Guntur <hrknd4@gmail.com>
2026-03-10 10:36:04 +0530

    new file added

--- 12-logs.sh
+++ 12-logs.sh
@@ -5,7 +5,7 @@ LOGS_FOLDER="/var/logs/shell-script"
 LOG_FILE="/var/logs/shell-script/$0.log"
 
 if [ $USERID -ne 0 ]; then
-    echo "Please run this script with root user access"
+    echo "Please run this script with root user access"| tee -a $LOG_FILE
     exit 1
 fi
 
@@ -14,10 +14,10 @@ mkdir -p $LOGS_FOLDER
 Validate(){
 
 if [ $1 -ne 0 ]; then
-    echo "$2 ... FAILURE"
+    echo "$2 ... FAILURE" | tee -a $LOG_FILE
     exit 1
 else
-    echo "$2 ... SUCCESS"
+    echo "$2 ... SUCCESS"| tee -a $LOG_FILE
 fi
 }
 
