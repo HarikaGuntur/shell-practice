40916bdb3c52e98144efc4eaf95ebee17d117c7e
Harika Guntur <hrknd4@gmail.com>
2026-03-11 17:45:07 +0530

    new file added

---
+++ 15-loops.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/logs/shell-script"
+LOG_FILE="/var/logs/shell-script/$0.log"
+
+if [ $USERID -ne 0 ]; then
+    echo "Please run this script with root user access"| tee -a $LOG_FILE
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+Validate(){
+
+if [ $1 -ne 0 ]; then
+    echo "$2 ... FAILURE" | tee -a $LOG_FILE
+    exit 1
+else
+    echo "$2 ... SUCCESS"| tee -a $LOG_FILE
+fi
+}
+
+for pacakage in $@ # sudo sh 14-loops.sh nginx mysql nodejs
+do 
+    dnf list installed $package &>>$LOG_FILE
+    if [ $? -ne 0]; then
+         echo "$pacakge not installed, installing now"
+        dnf install $pacakge -y &>>$LOG_FILE
+        Validate $? "$pacakge installation"
+    else 
+        echo "$package alaready installed, skipping"
+    fi
+done
