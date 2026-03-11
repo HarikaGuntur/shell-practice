463cbe8ddf0f12699f7fa386c413923a6c43c0d9
Harika Guntur <hrknd4@gmail.com>
2026-03-11 17:58:08 +0530

    new file added

--- 15-loops.sh
+++ 15-loops.sh
@@ -1,34 +1,33 @@
 #!/bin/bash
 
 USERID=$(id -u)
-LOGS_FOLDER="/var/logs/shell-script"
-LOG_FILE="/var/logs/shell-script/$0.log"
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/$0.log"
 
 if [ $USERID -ne 0 ]; then
-    echo "Please run this script with root user access"| tee -a $LOG_FILE
+    echo "Please run this script with root user access" | tee -a $LOGS_FILE
     exit 1
 fi
 
 mkdir -p $LOGS_FOLDER
 
-Validate(){
-
-if [ $1 -ne 0 ]; then
-    echo "$2 ... FAILURE" | tee -a $LOG_FILE
-    exit 1
-else
-    echo "$2 ... SUCCESS"| tee -a $LOG_FILE
-fi
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo "$2 ... FAILURE" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo "$2 ... SUCCESS" | tee -a $LOGS_FILE
+    fi
 }
 
 for package in $@ # sudo sh 14-loops.sh nginx mysql nodejs
-do 
-    dnf list installed $package &>>$LOG_FILE
+do
+    dnf list installed $package &>>$LOGS_FILE
     if [ $? -ne 0 ]; then
-         echo "$pacakge not installed, installing now"
-        dnf install $pacakge -y &>>$LOG_FILE
-        Validate $? "$package installation"
-    else 
+        echo "$package not installed, installing now"
+        dnf install $package -y &>>$LOGS_FILE
+        VALIDATE $? "$package installation"
+    else
         echo "$package already installed, skipping"
     fi
 done
